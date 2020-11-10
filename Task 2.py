080e728471de0103e56b895b59cf76c8c6240a0a
SHUBHANGI  RAIBOLE <67918969+ShubhangiRaibole@users.noreply.github.com>
2020-11-10 19:32:39 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Prediction using Unsupervised ML
+# 
+# #From the given ‘Iris’ dataset, predict the optimum number of clusters and
+# represent it visually.
+
+# In[34]:
+
+
+#import libraries
+
+
+# In[15]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler 
+from sklearn import datasets
+
+
+# In[19]:
+
+
+#Load Dataset
+lris = datasets.load_iris()
+
+lris=pd.DataFrame(lris.data, columns = lris.feature_names)
+
+
+# In[ ]:
+
+
+lris= pd.read_csv('')
+
+
+# In[20]:
+
+
+#check head 
+lris.head(10)
+
+
+# In[24]:
+
+
+#check pairplot
+import seaborn as sns
+sns.set(style="ticks",color_codes=True)
+g = sns.pairplot(lris,hue="sepal width (cm)")
+
+
+# # Perform Kmean Clustering
+
+# In[28]:
+
+
+# Finding the optimum number of clusters for k-means classification
+
+x = lris.iloc[:, [0, 1, 2, 3]].values
+
+from sklearn.cluster import KMeans
+wcss = []
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', 
+                    max_iter = 400, n_init = 10, random_state = 2)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+    
+# Plotting the results onto a line graph, 
+# `allowing us to observe 'The elbow'
+plt.plot(range(1, 11), wcss)
+plt.title('The elbow method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS') # Within cluster sum of squares
+plt.show()
+
+
+# In[29]:
+
+
+# Applying kmeans to the dataset / Creating the kmeans classifier
+kmeans = KMeans(n_clusters = 3, init = 'k-means++',
+                max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(x)
+
+
+# In[30]:
+
+
+# Visualising the clusters - On the first two columns
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], 
+            s = 100, c = 'red', label = 'Iris-setosa')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], 
+            s = 100, c = 'blue', label = 'Iris-versicolour')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1],
+            s = 100, c = 'green', label = 'Iris-virginica')
+
+# Plotting the centroids of the clusters
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], 
+            s = 100, c = 'yellow', label = 'Centroids')
+
+plt.legend()
+
+
+# In[32]:
+
+
+from __future__ import print_function
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+
+print(__doc__)
+
+# Generating the sample data from make_blobs
+# This particular setting has one distinct cluster and 3 clusters placed close
+# together.
+X=lris.values
+
+range_n_clusters = [2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15]
+
+for n_clusters in range_n_clusters:
+    # Create a subplot with 1 row and 2 columns
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.set_size_inches(18, 7)
+
+    # The 1st subplot is the silhouette plot
+    # The silhouette coefficient can range from -1, 1 but in this example all
+    # lie within [-0.1, 1]
+    ax1.set_xlim([-0.1, 1])
+    # The (n_clusters+1)*10 is for inserting blank space between silhouette
+    # plots of individual clusters, to demarcate them clearly.
+    ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+
+    # Initialize the clusterer with n_clusters value and a random generator
+    # seed of 10 for reproducibility.
+    clusterer = KMeans(n_clusters=n_clusters,n_init=15, random_state=2)
+    cluster_labels = clusterer.fit_predict(X)
+
+    # The silhouette_score gives the average value for all the samples.
+    # This gives a perspective into the density and separation of the formed
+    # clusters
+    silhouette_avg = silhouette_score(X, cluster_labels)
+    print("For n_clusters =", n_clusters,
+          "The average silhouette_score is :", silhouette_avg)
+
+    # Compute the silhouette scores for each sample
+    sample_silhouette_values = silhouette_samples(X, cluster_labels)
+
+    y_lower = 10
+    for i in range(n_clusters):
+        # Aggregate the silhouette scores for samples belonging to
+        # cluster i, and sort them
+        ith_cluster_silhouette_values =             sample_silhouette_values[cluster_labels == i]
+
+        ith_cluster_silhouette_values.sort()
+
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = cm.Spectral(float(i) / n_clusters)
+        ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                          0, ith_cluster_silhouette_values,
+                          facecolor=color, edgecolor=color, alpha=0.7)
+
+        # Label the silhouette plots with their cluster numbers at the middle
+        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+        # Compute the new y_lower for next plot
+        y_lower = y_upper + 10  # 10 for the 0 samples
+
+    ax1.set_title("The silhouette plot for the various clusters.")
+    ax1.set_xlabel("The silhouette coefficient values")
+    ax1.set_ylabel("Cluster label")
+
+    # The vertical line for average silhouette score of all the values
+    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+    ax1.set_yticks([])  # Clear the yaxis labels / ticks
+    ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    # 2nd Plot showing the actual clusters formed
+    colors = cm.Spectral(cluster_labels.astype(float) / n_clusters)
+    ax2.scatter(X[:, 0], X[:, 1], marker='.', s=30, lw=0, alpha=0.7,
+                c=colors)
+
+    # Labeling the clusters
+    centers = clusterer.cluster_centers_
+    # Draw white circles at cluster centers
+    ax2.scatter(centers[:, 0], centers[:, 1],
+                marker='o', c="white", alpha=1, s=200)
+
+    for i, c in enumerate(centers):
+        ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1, s=50)
+
+    ax2.set_title("The visualization of the clustered data.")
+    ax2.set_xlabel("Feature space for the 1st feature")
+    ax2.set_ylabel("Feature space for the 2nd feature")
+
+    plt.suptitle(("Silhouette analysis for KMeans clustering on sample data "
+                  "with n_clusters = %d" % n_clusters),
+                 fontsize=14, fontweight='bold')
+
+    plt.show()
+
+
+# In[33]:
+
+
+from scipy.cluster.hierarchy import dendrogram, linkage
+from scipy.spatial.distance import pdist
+plt.figure(figsize=(18, 16))
+plt.title('Agglomerative Hierarchical Clustering Dendogram')
+plt.xlabel('sample index')
+plt.ylabel('Distance')
+Z = linkage(lris, 'ward')
+dendrogram(Z,leaf_rotation=90.0,p=5,color_threshold=70,leaf_font_size=10,truncate_mode='level')
+plt.tight_layout()
+
+
+# # Thank you
